37379c8eabd1d3ee435a3090f26a1e3194efe557
Настя Скрицька <snastia.m@gmail.com>
2024-06-15 11:35:00 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,25 @@
+// tranding-movie
+// https://api.themoviedb.org/3/trending/movie/day?language=en-US
+
+// api_key=b5a20566eb3bb1e19d16b3f507e342a4
+
+// search
+// https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
+
+// потрібно там додати до пошукового рядка query=слово пошуку
+
+// деталі фільму
+// https://api.themoviedb.org/3/movie/movie_id?language=en-US'
+
+
+// акторський склад
+// https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US
+
+// Огляд фільму
+// https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1
+
+// там де вказано movie_id треба айдішник підставляти
+
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
@@ -13,6 +35,7 @@ export const App = () => {
 
         <Route path="/movie" element={<MoviePage/>}/>
         <Route path="/movie/:movieId" element={<MovieDetailPage/>}/>
+        <Route></Route>
       </Routes>
     </div>
   );

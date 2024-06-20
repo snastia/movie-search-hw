c42c602ac1ce0bb1972680efa7638aa4c2c63398
Настя Скрицька <snastia.m@gmail.com>
2024-06-20 13:56:21 +0300

    j

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 function HomePage(){
     const [trending, setTrending] = useState(null)
@@ -6,14 +7,16 @@ function HomePage(){
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=b5a20566eb3bb1e19d16b3f507e342a4`)
         .then(res => res.json())
-        .then(trending => setTrending(trending))
+        .then(trending => setTrending(trending.results))
         .catch(error => console.log(error))
-    })
+    }, [])
     return(
         <>
         <h1>Trending today</h1>
-        {trending && trending.map(() => {
-           
+        {trending && trending.map((movie) => {
+            return <li key={movie.id}>
+               <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            </li>
         })}
         </>
     )
